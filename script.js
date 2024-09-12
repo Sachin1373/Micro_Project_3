7e2d10648b5c3b75b6350ba3871f6def52acbb98
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-09-12 22:15:51 +0530

    Add a description of the changes

---
+++ script.js
@@ -0,0 +1,43 @@
+const input=document.getElementById('num');
+const buttons=document.querySelectorAll('button')
+
+let string = ""
+
+let arr=Array.from(buttons)
+
+arr.forEach(button =>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            try {
+                let result = eval(string);
+                
+                if (result % 1 !== 0) {
+                    result = result.toFixed(2); 
+                }
+                input.value = result;
+                string = result.toString(); 
+            } catch (error) {
+                input.value = "Invalid Input"; 
+                string = ""; 
+            }
+
+        }
+        else if(e.target.innerHTML == 'Reset'){
+            string=""
+            input.value=string
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+       else {
+    
+        if (e.target.innerHTML == 'x') {
+            string += '*';
+        } else {
+            string += e.target.innerHTML;
+        }
+        input.value = string;
+    }
+    })
+})
